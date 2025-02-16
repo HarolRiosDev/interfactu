9b8bf344aef20c21855dcbffe2c564288516c6e5
HRDev <125bharrio@gmail.com>
2025-02-16 22:58:21 +0100

    clear

--- android/app/src/main/kotlin/com/example/interfactu/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.interfactu
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/config.dart
+++
@@ -1,3 +0,0 @@
-class Config {
-  static const String endpointUrl = 'http://interfactu.com/api/DemoQRCode';
-}
